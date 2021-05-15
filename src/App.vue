8b6bccaa3b482c55c360b9213b8e71fc5ca28bb9
TobiOjuolape <tobilobaojuolape@gmail.com>
2021-05-15 13:11:15 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main style="background:#e5e5e5">
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-form>
+    <v-card class="pa-3" width="600px" style="margin:4% auto 10% auto;max-width:100%">
+    <v-card>
+    <v-img src="https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwiRtuiNz8vwAhXOgVwKHdSAD7UQPAgI" />
+    <v-select outlined
+    :items="items"
+    chips
+    label="Select ID Type"
+     />
+    </v-card>
+    
+    
+    </v-card>
+    
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+     items:['Bank Verificationn Number (BVN)', 'Corporate Affairs Commission (CAC)', 'National Identity Number (NIN)']
+    }),
+  }
+</script>
